b6d6e7ebc392561a3ea20de8c387158ad300d40e
Deepak Bhagat <deepak.bhagat@publicissapient.net>
2021-08-11 00:20:13 +0530

    Add 1st program

--- Java_stream_Program/src/com/java8/stream/programs/MapMethod.java
+++ Java_stream_Program/src/com/java8/stream/programs/MapMethod.java
@@ -1,9 +1,14 @@
 package com.java8.stream.programs;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MapMethod {
 
 	public static void main(String[] args) {
-
+		List<Integer> list = Arrays.asList(3, 6, 9, 12, 15);
+		
+        list.stream().map(number -> number * 3).forEach(System.out::println);
 	}
 
 }
